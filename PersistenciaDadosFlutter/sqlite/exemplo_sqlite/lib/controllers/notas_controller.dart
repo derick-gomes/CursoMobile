02fb0629ca3636fd5d4a66093cdf2ca49006d3ee
derick-gomes <derickpizza@gmail.com>
2025-05-15 16:57:11 -0300

    15/05/2025

---
+++ PersistenciaDadosFlutter/sqlite/exemplo_sqlite/lib/controllers/notas_controller.dart
@@ -0,0 +1,22 @@
+import 'package:exemplo_sqlite/models/notas_model.dart';
+import 'package:exemplo_sqlite/services/nota_dbhelper.dart';
+
+class NotasController {
+  final NotaDBhelper _dBhelper = NotaDBhelper();
+
+  //4 métodos do crud
+
+  Future<List<Nota>> fetchNotas() async {
+    return await _dBhelper.getNotas();
+  }
+
+  Future<int> addNota(Nota nota) async {
+    return await _dBhelper.insertNota(nota);
+  } // retorna id da linha inserida
+
+  Future<int> updateNota(Nota nota) async {
+    return await _dBhelper.updateNota(nota);
+  }
+
+  
+}
